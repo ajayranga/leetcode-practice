e72551d7570b964a68746e9d34523a1dcf0e3acc
Ajay <117063355+ajayranga@users.noreply.github.com>
2024-03-24 18:20:32 +0530

    Time: 77 ms (38.77%) | Memory: 54.3 MB (66.99%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.ts
@@ -0,0 +1,19 @@
+function topKFrequent(nums: number[], k: number): number[] {
+  const ans: number[] = [];
+  const frqMap = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    frqMap.set(nums[i], frqMap.get(nums[i]) ? frqMap.get(nums[i])! + 1 : 1);
+  }
+  const frqArr = Array.from(frqMap);
+  for (let i = 0; i < k; i++) {
+    for (let j = i + 1; j < frqArr.length; j++) {
+      if (frqArr[i][1] < frqArr[j][1]) {
+        let temp = frqArr[i];
+        frqArr[i] = frqArr[j];
+        frqArr[j] = temp;
+      }
+    }
+    ans.push(frqArr[i][0]);
+  }
+  return ans;
+}
