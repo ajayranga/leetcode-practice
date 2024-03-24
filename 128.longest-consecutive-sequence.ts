961935dba248e576602638daaa5f26944a1c94b7
ajay <ramgaa307@gmail.com>
2024-03-24 13:14:55 +0530

    128 nlogn

--- 128.longest-consecutive-sequence.ts
+++ 128.longest-consecutive-sequence.ts
@@ -6,28 +6,26 @@
 
 // @lc code=start
 function longestConsecutive(nums: number[]): number {
-  let maxConsLen = 0;
+  let maxConsLen = 1;
   nums.sort((a, b) => a - b);
-  for (let i = 0; i < nums.length; i++) {
-    let currLen = 1;
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] === nums[j]) {
-        i++;
-        continue;
-      }
-      if (nums[i] === nums[j] - 1) {
-        currLen++;
-        i++;
-      } else {
-        i = --j;
-        break;
-      }
+  let smlIdx = 0;
+  let currLen = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i - 1] === nums[i]) {
+      continue;
     }
-    maxConsLen = Math.max(maxConsLen, currLen);
+    if (nums[smlIdx] + currLen === nums[i]) {
+      currLen++;
+      maxConsLen = Math.max(maxConsLen, currLen);
+    } else {
+      currLen = 1;
+      smlIdx = i;
+    }
   }
-  return maxConsLen;
+  return nums.length === 0 ? 0 : maxConsLen;
 }
 // @lc code=end
 // console.clear();
 // console.log(longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]));
 // console.log(longestConsecutive([9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6]));
+// console.log(longestConsecutive([1, 2, 0, 1]));
