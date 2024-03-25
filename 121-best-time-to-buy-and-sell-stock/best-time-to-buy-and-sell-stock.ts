6dcc4036dd830e8f8d7ce75c7b750d39855dd069
Ajay <117063355+ajayranga@users.noreply.github.com>
2024-03-25 20:25:15 +0530

    Time: 80 ms (54.60%) | Memory: 60.1 MB (25.82%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,15 @@
+function maxProfit(prices: number[]): number {
+  let ans = 0;
+  let l = 0,
+    r = 1;
+  while (r < prices.length) {
+    if (prices[l] > prices[r]) {
+      l++;
+      r = l + 1;
+    } else {
+      ans = Math.max(ans, prices[r] - prices[l]);
+      r++;
+    }
+  }
+  return ans;
+}
