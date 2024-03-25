7419ada8a2ed3dffe66842cea12ce7bfb16e7bb5
ajay <ajay.ranga.mr.unique@gmail.com>
2024-03-25 21:08:03 +0530

    3 efficient

---
+++ 3.longest-substring-without-repeating-characters.ts
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=3 lang=typescript
+ *
+ * [3] Longest Substring Without Repeating Characters
+ */
+
+// @lc code=start
+function lengthOfLongestSubstring(s: string): number {
+  if (s.length < 2) {
+    return s.length;
+  }
+  let l = 0;
+  let ans = 0;
+  const mySet = new Set<string>();
+  for (let i = 0; i < s.length; i++) {
+    while (mySet.has(s[i])) {
+      mySet.delete(s[l]);
+      l++;
+    }
+    mySet.add(s[i]);
+    ans = Math.max(ans, i - l + 1);
+  }
+  return ans;
+}
+// @lc code=end
