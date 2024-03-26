ca8e3278c218cd2c36fc4833bf333936069a7a36
ajay <ajay.ranga.mr.unique@gmail.com>
2024-03-26 13:09:18 +0530

    73 done

---
+++ 73.set-matrix-zeroes.ts
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=73 lang=typescript
+ *
+ * [73] Set Matrix Zeroes
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function setZeroes(matrix: number[][]): void {
+  const zeroContainingRow: number[] = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === 0) {
+        zeroContainingRow.push(i, j);
+      }
+    }
+  }
+  for (let i = 0; i < zeroContainingRow.length; i += 2) {
+    for (let j = 0; j < matrix[zeroContainingRow[i]].length; j++) {
+      matrix[zeroContainingRow[i]][j] = 0;
+    }
+  }
+  for (let i = 0; i < zeroContainingRow.length; i += 2) {
+    for (let j = 0; j < matrix.length; j++) {
+      matrix[j][zeroContainingRow[i + 1]] = 0;
+    }
+  }
+}
+// @lc code=end
