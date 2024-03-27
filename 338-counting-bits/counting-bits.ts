cbbfdfb9a7a1f92dd72d8f27395ff91d6bbd9de3
Ajay <117063355+ajayranga@users.noreply.github.com>
2024-03-27 10:26:54 +0530

    Time: 68 ms (90.36%) | Memory: 57.4 MB (43.25%) - LeetSync

--- 338-counting-bits/counting-bits.ts
+++ 338-counting-bits/counting-bits.ts
@@ -1,18 +1,7 @@
 function countBits(n: number): number[] {
-    const ans: number[] = [];
-    let curr = 0;
-
-    while (curr !== n + 1) {
-        let temp = curr;
-        let cnt1s = 0;
-        while (temp) {
-            if ((temp & 1) === 1) {
-                cnt1s++;
-            }
-            temp = temp >> 1;
-        }
-        ans.push(cnt1s);
-        curr++;
+    const ans: number[] = [0];
+    for (let i = 1; i <= n; i++) {
+        ans[i] = ans[i >> 1] + i % 2
     }
     return ans;
 }
