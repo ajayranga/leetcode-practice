22f3c399174ea87546a43c865e0e501a94c38559
Ajay <117063355+ajayranga@users.noreply.github.com>
2024-03-27 23:13:49 +0530

    Time: 61 ms (83.26%) | Memory: 52 MB (13.52%) - LeetSync

--- 2859-add-two-promises/add-two-promises.ts
+++ 2859-add-two-promises/add-two-promises.ts
@@ -1,15 +1,21 @@
 type P = Promise<number>
 
 async function addTwoPromises(promise1: P, promise2: P): P {
-    return (await promise1)+(await promise2);
+    // return (await promise1) + (await promise2);
     // return new Promise((resolve) => {
     //     Promise.allSettled([promise1, promise2]).then(result => {
     //         let ans: number = 0;
     //         result.forEach(res => ans += res.value);
     //         resolve(ans)
     //     })
-
     // })
+    return new Promise((resolve) => {
+        Promise.all([promise1, promise2]).then(result => {
+            let ans: number = 0;
+            result.forEach(res => ans += res);
+            resolve(ans)
+        })
+    })
 };
 
 /**
