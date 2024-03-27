8a98152723698fd9cc84d000d8e444c02931d3f1
Ajay <117063355+ajayranga@users.noreply.github.com>
2024-03-27 23:00:35 +0530

    Time: 335 ms (17.29%) | Memory: 90.6 MB (31.74%) - LeetSync

---
+++ 2731-memoize/memoize.ts
@@ -0,0 +1,24 @@
+type Fn = (...params: number[]) => number
+
+function memoize(fn: Fn): Fn {
+    const cacheMap = new Map();
+    return function (...args) {
+        const caller = [fn.toString(), ...args].join("-")
+        if (!cacheMap.has(caller)) {
+            cacheMap.set(caller, fn(...args))
+        }
+        return cacheMap.get(caller)
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
