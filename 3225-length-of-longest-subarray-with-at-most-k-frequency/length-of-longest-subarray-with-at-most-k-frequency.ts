ee6bd1c532439eb9e3a5e3a03f58dea5fbcaa6fd
Ajay <117063355+ajayranga@users.noreply.github.com>
2024-03-28 13:05:51 +0530

    Time: 152 ms (73.53%) | Memory: 71.5 MB (41.18%) - LeetSync

---
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.ts
@@ -0,0 +1,22 @@
+function maxSubarrayLength(nums: number[], k: number): number {
+    // if (nums.length < 2) {
+    //     return nums.length;
+    // }
+    let ans = 0;
+    let frqMap = new Map<number, number>();
+    let l = 0;
+    // frqMap.set(nums[0], 1)
+    for (let i = 0; i < nums.length; i++) {
+        if (frqMap.has(nums[i])) {
+            frqMap.set(nums[i], frqMap.get(nums[i]) + 1)
+            while (frqMap.get(nums[i]) > k) {
+                frqMap.set(nums[l], frqMap.get(nums[l]) - 1)
+                l++;
+            }
+        } else {
+            frqMap.set(nums[i], 1)
+        }
+        ans = Math.max(ans, i - l + 1)
+    }
+    return ans;
+};
