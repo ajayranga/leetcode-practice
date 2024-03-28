1431f7fb05b7cb7f63f928e0754d47b7416b5efb
Ajay <117063355+ajayranga@users.noreply.github.com>
2024-03-28 12:34:09 +0530

    Time: 40 ms (99.39%) | Memory: 51.5 MB (78.87%) - LeetSync

---
+++ 2805-array-wrapper/array-wrapper.ts
@@ -0,0 +1,22 @@
+class ArrayWrapper {
+    arr;
+    constructor(nums: number[]) {
+        this.arr = nums
+    }
+
+    valueOf(): number {
+        return this.arr.reduce((accu, curr) => accu += curr, 0);
+    }
+
+    toString(): string {
+        return `[${this.arr.join(",")}]`
+    }
+};
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
