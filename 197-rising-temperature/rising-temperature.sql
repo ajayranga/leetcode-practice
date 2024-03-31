d23239b6c78bf45de8b78dd54a0e095226d0056f
Ajay <117063355+ajayranga@users.noreply.github.com>
2024-03-31 16:56:56 +0530

    Time: 536 ms (53.80%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,5 +1,5 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT W2.Id FROM WEATHER W1
 JOIN WEATHER W2
-ON DATEDIFF( W2.recordDate ,W1.recordDate) = 1 AND
+ON W2.recordDate::DATE - W1.recordDate::DATE = 1 AND
 W2.temperature>W1.temperature
