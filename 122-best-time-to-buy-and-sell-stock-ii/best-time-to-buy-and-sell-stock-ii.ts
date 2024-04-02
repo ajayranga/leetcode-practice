942c5c4f4859999fa6250057f984eb612ab49800
Ajay <117063355+ajayranga@users.noreply.github.com>
2024-04-02 13:37:04 +0530

    Time: 56 ms (77.60%) | Memory: 51.7 MB (64.52%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.ts
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.ts
@@ -1,16 +1,11 @@
 function maxProfit(prices: number[]): number {
-    // if (prices.length === 2) {
-    //     if (prices[0] < prices[1]) {
-    //         return prices[1] - prices[0];
-    //     }
-    // }
     let l = 0, r = 1, maxProfit = 0, bought = 0, havBought = false;
     while (r <= prices.length) {
         if (prices[l] < prices[r]) {
             if (!havBought) {
                 havBought = true;
                 bought = prices[l];
-            } 
+            }
         } else {
             if (havBought) {
                 havBought = false;
@@ -21,8 +16,8 @@ function maxProfit(prices: number[]): number {
         l++;
         r++;
     }
-                if (havBought) {
-                    maxProfit += prices[r-1] - bought;
-                }
+    if (havBought) {
+        maxProfit += prices[r - 1] - bought;
+    }
     return maxProfit;
 };
