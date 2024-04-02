db949ff48624dcf974ff67969c7716918d7a578a
Ajay <117063355+ajayranga@users.noreply.github.com>
2024-04-02 22:02:17 +0530

    Time: 62 ms (31.04%) | Memory: 50.9 MB (50.76%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.ts
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.ts
@@ -1,6 +1,6 @@
 function strStr(haystack: string, needle: string): number {
     let j = 0;
-    for (let i = 0; i < haystack.length; i++) {
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
         if (haystack[i] === needle[j]) {
             let matched = true;
             for (let k = 1; k < needle.length; k++) {
