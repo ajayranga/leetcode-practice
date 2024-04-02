48f41def2a0dd02ff06051ae1784026a82416b9a
Ajay <117063355+ajayranga@users.noreply.github.com>
2024-04-02 22:00:17 +0530

    Time: 62 ms (31.04%) | Memory: 51.8 MB (11.43%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.ts
@@ -0,0 +1,18 @@
+function strStr(haystack: string, needle: string): number {
+    let j = 0;
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack[i] === needle[j]) {
+            let matched = true;
+            for (let k = 1; k < needle.length; k++) {
+                if (haystack[i + k] !== needle[k]) {
+                    matched = false
+                    break;
+                }
+            }
+            if (matched) {
+                return i
+            }
+        }
+    }
+    return -1;
+};
