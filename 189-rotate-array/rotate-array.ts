a0a3fba75f1cb052cc2f2a71a7c80ca458f2e935
Ajay <117063355+ajayranga@users.noreply.github.com>
2024-04-02 12:25:56 +0530

    Time: 84 ms (80.88%) | Memory: 59.4 MB (53.05%) - LeetSync

--- 189-rotate-array/rotate-array.ts
+++ 189-rotate-array/rotate-array.ts
@@ -2,6 +2,23 @@
  Do not return anything, modify nums in-place instead.
  */
 function rotate(nums: number[], k: number): void {
-    for (let i = 0; i < k % nums.length; i++)
-        nums.unshift(nums.pop())
+    // for (let i = 0; i < k % nums.length; i++)
+    //     nums.unshift(nums.pop())
+    reverse(nums, 0, nums.length - 1)
+    reverse(nums, 0, k % nums.length - 1)
+    reverse(nums, k % nums.length, nums.length - 1)
 };
+const reverse = (arr: number[], start: number, end: number) => {
+    while (start < end) {
+        arr[start] = arr[start] + arr[end]
+        arr[end] = arr[start] - arr[end]
+        arr[start] = arr[start] - arr[end]
+        start++;
+        end--;
+    }
+}
+
+// [1,2,3,4,5,6,7]
+// [7,6,5,4,3,2,1]
+// [5,6,7,4,3,2,1]
+// [5,6,7,4,3,2,1]
